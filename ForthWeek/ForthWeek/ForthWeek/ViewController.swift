831ab40b790ab94bf94d058df93e8a98373ef1ec
非典型技术宅 <3840553@qq.com>
2017-11-09 22:19:06 +0800

    update Demo17

---
+++ ForthWeek/ForthWeek/ForthWeek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForthWeek
+//
+//  Created by Stan on 2017-11-09.
+//  Copyright © 2017 Stan Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
