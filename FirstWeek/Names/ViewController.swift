c7f710ceca8e63b1b6ccc1779ca17420b64f6128
Stan Guo <3840553@qq.com>
2018-01-31 23:11:55 +0800

    update Name List

--- FirstWeek/Names/ViewController.swift
+++ FirstWeek/Names/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Names
 //
-//  Created by Stan on 2017-10-17.
+//  Created by Stan on 2018-01-31.
 //  Copyright © 2017 stan. All rights reserved.
 //
 
@@ -10,18 +10,20 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    fileprivate let cellID = "tableViewCellID"
+    private let cellID = "tableViewCellID"
     
-    fileprivate var path: String {
+    private var path: String {
         get {
             let path = Bundle.main.path(forResource: "names", ofType: "plist")
             return path ?? ""
         }
     }
-    var indexArray: [String] {
+    
+    
+    /// get a dictionary Array from plist
+    private var indexArray: [String] {
         get {
             guard let arr = NSDictionary.init(contentsOfFile: path)?.allKeys as? [String] else { return [String]() }
-            
             return arr.sorted()
         }
     }
@@ -29,18 +31,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
-        
-        
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     //    MARK: - Lazy
     fileprivate lazy var tableView: UITableView = {
@@ -59,30 +51,27 @@ extension ViewController: UITableViewDataSource,UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-
         guard let arr = NSDictionary.init(contentsOfFile: path)?.value(forKey: indexArray[section]) as? [String] else { return 0 }
-        
         return arr.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        
 
         guard let arr = NSDictionary.init(contentsOfFile: path)?.value(forKey: indexArray[indexPath.section]) as? [String] else { return cell}
-        
-        
         cell.textLabel?.text = arr[indexPath.row].description
         return cell
     }
+    
     
     func sectionIndexTitles(for tableView: UITableView) -> [String]? {
         return indexArray
     }
     
+    
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return indexArray[section]
     }
+    
 }
 
